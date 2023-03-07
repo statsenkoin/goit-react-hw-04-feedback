38d5889c097f9bcdb5e64c39016e71ac46e750ea
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-07 16:29:09 +0200

    Fix/ rename callback

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Option, OptionList } from './FeedbackOptions.styled';
 
-const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+const FeedbackOptions = ({ options, addFeedback }) => {
   return (
     <OptionList>
       {options.map(option => {
         return (
-          <Option key={option} id={option} onClick={onLeaveFeedback}>
+          <Option key={option} id={option} onClick={addFeedback}>
             {option}
           </Option>
         );
@@ -20,5 +20,5 @@ export { FeedbackOptions };
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onLeaveFeedback: PropTypes.func.isRequired,
+  addFeedback: PropTypes.func.isRequired,
 };
