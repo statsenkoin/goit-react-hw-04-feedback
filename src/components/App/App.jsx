472ae4806a78fdbc8b26f9562278352fa27cff7b
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-07 16:30:34 +0200

    Feature/ class is refactored to function useing hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,65 +1,59 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Layout } from './App.styled';
 import { Section, FeedbackOptions, Statistics, Notification } from 'components';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [totalFeedback, setTotalFeedback] = useState(0);
+  const [feedbackPercentage, setFeedbackPercentage] = useState(0);
+
+  const addFeedback = e => {
+    const { id } = e.target;
+    switch (id) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        throw new Error('Unsupported feedback option');
+    }
   };
 
-  totalFeedback = 0;
-  positiveFeedbackPercentage = 0;
+  useEffect(() => {
+    const total = good + neutral + bad;
+    setTotalFeedback(() => total);
+    setFeedbackPercentage(() => Number(((good / total) * 100).toFixed(0)));
+  }, [bad, good, neutral]);
 
-  addFeedback = event => {
-    const { id } = event.target;
-    this.setState(prevState => {
-      return { [id]: prevState[id] + 1 };
-    });
-  };
+  return (
+    <Layout>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          addFeedback={addFeedback}
+        ></FeedbackOptions>
+      </Section>
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    this.totalFeedback = good + neutral + bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    this.positiveFeedbackPercentage = Number(
-      ((this.state.good / this.totalFeedback) * 100).toFixed(0)
-    );
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
-
-    return (
-      <Layout>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.addFeedback}
-          ></FeedbackOptions>
-        </Section>
-
-        <Section title="Statistics">
-          {this.totalFeedback ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.totalFeedback}
-              positivePercentage={this.positiveFeedbackPercentage}
-            ></Statistics>
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </Layout>
-    );
-  }
+      <Section title="Statistics">
+        {totalFeedback ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={feedbackPercentage}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </Layout>
+  );
 }
-
-export { App };
